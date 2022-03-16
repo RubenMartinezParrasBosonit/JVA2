541c3c7aefab4ec905776c16990111a9d28b06c1
Rubén Martínez Parras <ruben.mparras@bosonit.com>
2022-03-16 11:22:49 +0100

    entidad files añadido

---
+++ src/main/java/io/github/ruben/files/application/FileService.java
@@ -0,0 +1,22 @@
+package io.github.ruben.files.application;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public interface FileService {
+
+    void init();
+
+    void store(MultipartFile file);
+
+    Stream<Path> loadAll();
+
+    Path load(String filename);
+
+    Resource loadAsResource(String filename);
+
+    void deleteAll();
+}
